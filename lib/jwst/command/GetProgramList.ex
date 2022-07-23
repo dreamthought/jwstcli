7e0b5b7f551af7c1378e4e0dd7c1be0b66e409c8
Raf Gemmail <rafiq@dreamthought.com>
2022-07-24 11:59:42 +1200

    Mapped external response to just program ids

--- lib/jwst/command/GetProgramList.ex
+++ lib/jwst/command/GetProgramList.ex
@@ -3,7 +3,16 @@ defmodule Jwst.Command.GetProgramList do
   require Logger
 
   def execute(api_key) do
-   Jwst.Api.get_program_list(api_key)
+    {result, response} = Jwst.Api.get_program_list(api_key)
+
+    unless :ok = result do
+      # TODO - ensure that this does not leak security info
+      Logger.critical inspect response, "Failure"
+      raise "Failed to query get_program_list"
+    end
+
+    {_,body} = Jason.decode(response.body)
+    Enum.map(body["body"], fn map -> map["program"] end)
   end
 
 end
